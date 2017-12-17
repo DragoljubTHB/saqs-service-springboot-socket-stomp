b2b825b5afdd9cbe822402e0a6dbabfcbb0abd7f
dado <littlevwing@live.com>
2017-12-17 18:42:04 +0100

    initial

---
+++ src/main/java/saqs/eis/StationsController.java
@@ -0,0 +1,17 @@
+package saqs.eis;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class StationsController {
+
+    @MessageMapping("/stations")
+    @SendTo("/basestations")
+    public String greeting(String message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return "hello!";
+    }
+
+}
