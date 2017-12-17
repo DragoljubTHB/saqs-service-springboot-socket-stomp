2bc24912d9d8006d906341d9b0f087e9b289d2a9
DragoljubTHB <littlevwing@live.com>
2017-12-17 22:24:18 +0100

    updated CORS

--- src/main/java/saqs/eis/StationsRESTController.java
+++ src/main/java/saqs/eis/StationsRESTController.java
@@ -1,6 +1,7 @@
 package saqs.eis;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,6 +13,7 @@ import java.util.List;
 @RequestMapping("/stations")
 public class StationsRESTController {
 
+    @CrossOrigin(origins = "*")
     @RequestMapping(method = RequestMethod.GET)
     public @ResponseBody
     List<Station> getAllStations() {
