3441df9a80ade213cf3def941f9d5843d86fd5ba
DragoljubTHB <littlevwing@live.com>
2017-12-18 00:10:53 +0100

    added layers in between controller and model

---
+++ src/main/java/saqs/eis/IStationRepository.java
@@ -0,0 +1,4 @@
+package saqs.eis;
+
+public interface IStationRepository {
+}
---
+++ src/main/java/saqs/eis/StationDTO.java
@@ -0,0 +1,7 @@
+package saqs.eis;
+
+import java.text.SimpleDateFormat;
+
+public class StationDTO {
+    private static final SimpleDateFormat date = new SimpleDateFormat("dd-MM-yyyy");
+}
