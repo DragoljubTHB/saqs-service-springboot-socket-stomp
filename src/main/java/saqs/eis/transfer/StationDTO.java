9fd32b0d84e6f686a19f4dc6464be57f4b9bc148
DragoljubTHB <littlevwing@live.com>
2017-12-18 02:00:25 +0100

    added buggy colour logic

--- src/main/java/saqs/eis/transfer/StationDTO.java
+++ src/main/java/saqs/eis/transfer/StationDTO.java
@@ -6,5 +6,61 @@ import java.text.SimpleDateFormat;
  * to batch up what would be multiple remote calls into a single one
  */
 public class StationDTO {
-    private static final SimpleDateFormat date = new SimpleDateFormat("dd-MM-yyyy");
+    private Integer variance;
+    private String colour;
+
+    private String name;
+    private Integer target;
+    private Integer actual;
+    private String date;
+
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public Integer getVariance() {
+        return variance;
+    }
+
+    public void setVariance(Integer variance) {
+        this.variance = variance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getTarget() {
+        return target;
+    }
+
+    public void setTarget(Integer target) {
+        this.target = target;
+    }
+
+    public Integer getActual() {
+        return actual;
+    }
+
+    public void setActual(Integer actual) {
+        this.actual = actual;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
 }
